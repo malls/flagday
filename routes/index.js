083514a16787c86b9eb15c71f15545f27a35230f
Forrest Almasi <forrest.almasi@gmail.com>
2022-06-14 21:45:47 -0400

    rerun logic on fetch

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-var today = new Date();
-var month = today.getMonth();
-var day = today.getDate();
 
 /* GET home page. */
 router.get('/', function(req, res) {
+    var today = new Date();
+    var month = today.getMonth();
+    var day = today.getDate();
+    
     if (month === 5 && day === 14) {
         res.render('yes');
     } else {
